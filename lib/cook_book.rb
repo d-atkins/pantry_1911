617991a65d83fee44c4a375a1e21bba024e05e56
David Atkins <atki1080@gmail.com>
2020-01-08 11:52:24 -0700

    Add summary method, fix sorting in ingredient_report

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -11,7 +11,9 @@ class CookBook
 
   def ingredient_report(recipe)
     report = []
-    sorted_ingredients = recipe.ingredients.sort_by{|ingredient| ingredient.calories}
+    sorted_ingredients = recipe.ingredients.sort_by do |ingredient|
+      1 / (ingredient.calories * recipe.ingredients_required[ingredient]).to_f
+    end
     sorted_ingredients.each do |ingredient|
       amount_string = "#{recipe.ingredients_required[ingredient]} #{ingredient.unit}"
       report << {ingredient: ingredient.name, amount: amount_string}
@@ -19,7 +21,16 @@ class CookBook
     report
   end
 
-  # def summary
-  #   report = []
-  # end
+  def summary
+    report = []
+    @recipes.each do |recipe|
+      recipe_report = Hash.new
+      recipe_report[:name] = recipe.name
+      recipe_report[:details] = Hash.new
+      recipe_report[:details][:ingredients] = ingredient_report(recipe)
+      recipe_report[:details][:total_calories] = recipe.total_calories
+      report << recipe_report
+    end
+    report
+  end
 end
