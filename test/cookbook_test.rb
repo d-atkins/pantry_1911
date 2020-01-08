1cd011bc54860c5fa826fbd3dc18d4104ae081aa
David Atkins <atki1080@gmail.com>
2020-01-08 11:00:39 -0700

    Add CookBook test suite

---
+++ test/cookbook_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/cook_book'
+
+class CookbookTest < Minitest::Test
+  def test_it_exists
+    cookbook = CookBook.new
+    assert_instance_of CookBook, cookbook
+  end
+end
