b47edabcaf93d1c3d877d48b3f9534b42a8f4321
David Atkins <atki1080@gmail.com>
2020-01-08 10:21:14 -0700

    Add add_ingredients test case

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -16,4 +16,14 @@ class RecipeTest < Minitest::Test
     assert_equal "Mac and Cheese", @recipe.name
     assert_equal ({}), @recipe.ingredients_required
   end
+
+  def test_it_can_add_ingredients
+    ingredient1 = Ingredient.new("Cheese", "C", 100)
+    ingredient2 = Ingredient.new("Macaroni", "oz", 30)
+    @recipe.add_ingredient(ingredient1, 2)
+    @recipe.add_ingredient(ingredient2, 8)
+    expected = {ingredient1 => 2, ingredient2 => 8}
+    
+    assert_equal expected, @recipe.ingredients_required
+  end
 end
