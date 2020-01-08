31ee6af1a0479c61754df99de5fe5c3d7d9d3add
David Atkins <atki1080@gmail.com>
2020-01-08 10:39:39 -0700

    Add attributes test case

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -5,8 +5,15 @@ require './lib/recipe'
 require './lib/pantry'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
+
   def test_it_exists
-    pantry = Pantry.new
-    assert_instance_of Pantry, pantry
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_it_has_attributes
+    assert_equal ({}), @pantry.stock 
   end
 end
