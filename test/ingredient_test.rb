8c912ecfc655a281aeb05a4f1292ef8a2fd28f31
David Atkins <atki1080@gmail.com>
2020-01-08 10:07:34 -0700

    Add attributes test case

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -3,8 +3,17 @@ require 'minitest/pride'
 require './lib/ingredient.rb'
 
 class IngredientTest < Minitest::Test
+  def setup
+    @ingredient = Ingredient.new("Cheese", "oz", 50)
+  end
+
   def test_it_exists
-    ingredient = Ingredient.new("Cheese", "oz", 50)
-    assert_instance_of Ingredient, ingredient
+    assert_instance_of Ingredient, @ingredient
+  end
+
+  def test_it_has_attributes
+    assert_equal "Cheese", @ingredient.name
+    assert_equal "oz", @ingredient.unit
+    assert_equal 50, @ingredient.calories
   end
 end
