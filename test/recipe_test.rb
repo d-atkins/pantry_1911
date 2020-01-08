237d2805ed43c96f68774a5ae5a924b2c966e3c5
David Atkins <atki1080@gmail.com>
2020-01-08 10:17:08 -0700

    Add attributes test case

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -4,8 +4,16 @@ require './lib/ingredient'
 require './lib/recipe'
 
 class RecipeTest < Minitest::Test
+  def setup
+    @recipe = Recipe.new("Mac and Cheese")
+  end
+
   def test_it_exists
-    recipe = Recipe.new("Mac and Cheese")
-    assert_instance_of Recipe, recipe
+    assert_instance_of Recipe, @recipe
+  end
+
+  def test_it_has_attributes
+    assert_equal "Mac and Cheese", @recipe.name
+    assert_equal ({}), @recipe.ingredients_required
   end
 end
